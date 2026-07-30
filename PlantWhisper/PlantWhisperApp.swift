f456269f778141eeb03261187702b6027b13c920
vikramsingh <vc7662@gmail.com>
2026-07-30 21:18:47 +0530

    Initial Commit

---
+++ PlantWhisper/PlantWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantWhisperApp.swift
+//  PlantWhisper
+//
+//  Created by Vikram Singh Depawat on 30/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PlantWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
